d08f3cee709e30f3a28f6488214a4bb604f8c5d7
KolaYAndr <nikolay_andreev02@mail.ru>
2024-06-29 02:55:23 +0300

    Переехал на композ

---
+++ app/src/main/java/com/cleverpumpkin/todoapp/presentation/navigation/Navigation.kt
@@ -0,0 +1,65 @@
+package com.cleverpumpkin.todoapp.presentation.navigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.cleverpumpkin.todoapp.presentation.screens.todo_detail_screen.TodoDetailScreen
+import com.cleverpumpkin.todoapp.presentation.screens.todo_detail_screen.TodoDetailViewModel
+import com.cleverpumpkin.todoapp.presentation.screens.todo_list_screen.TodoListScreen
+import com.cleverpumpkin.todoapp.presentation.screens.todo_list_screen.TodoListViewModel
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = NavRoutes.TODO_LIST_SCREEN
+    ) {
+        composable(route = NavRoutes.TODO_LIST_SCREEN) {
+            val todoListViewModel = hiltViewModel<TodoListViewModel>()
+            val state = todoListViewModel.uiState.collectAsStateWithLifecycle()
+            TodoListScreen(
+                state = state,
+                onEndToStartAction = { item -> todoListViewModel.deleteItem(item) },
+                onAddItem = { navController.navigate("${NavRoutes.TODO_DETAIL_SCREEN}/${NavArgs.CREATE_TODO}") },
+                onNavigate = { id -> navController.navigate("${NavRoutes.TODO_DETAIL_SCREEN}/${id}") },
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+
+        composable(
+            route = "${NavRoutes.TODO_DETAIL_SCREEN}/{${NavArgs.TODO_ID}}",
+            arguments = listOf(
+                navArgument(NavArgs.TODO_ID) {
+                    type = NavType.StringType
+                    nullable = false
+                }
+            )
+        ) { backStackEntry ->
+            val todoDetailViewModel = hiltViewModel<TodoDetailViewModel>()
+            val id = backStackEntry.arguments?.getString(NavArgs.TODO_ID) ?: NavArgs.CREATE_TODO
+            todoDetailViewModel.findItem(id)
+            val state = todoDetailViewModel.uiState.collectAsStateWithLifecycle()
+            TodoDetailScreen(
+                state = state,
+                onSave = {
+                    todoDetailViewModel.addItem()
+                    navController.navigateUp()
+                },
+                onNavBack = { navController.navigateUp() },
+                onSelectImportance = { todoDetailViewModel.selectImportance(it) },
+                onDelete = {
+                    todoDetailViewModel.deleteItem()
+                    navController.navigateUp()
+                },
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+    }
+}
