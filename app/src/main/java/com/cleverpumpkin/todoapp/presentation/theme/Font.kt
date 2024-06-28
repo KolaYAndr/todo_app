64fca785c1fab366fb1737461eb4c76a5cb6404d
KolaYAndr <nikolay_andreev02@mail.ru>
2024-06-29 02:58:08 +0300

    Темизация + превью палитры

---
+++ app/src/main/java/com/cleverpumpkin/todoapp/presentation/theme/Font.kt
@@ -0,0 +1,9 @@
+package com.cleverpumpkin.todoapp.presentation.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.cleverpumpkin.todoapp.R
+
+val RobotoFamily = FontFamily(
+    Font(R.font.roboto)
+)
